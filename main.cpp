9a2fcb0f56d062f6aacd924c250c6e9da0acb874
Alex Brochu <alexbrochu1@gmail.com>
2022-01-01 22:25:31 -0500

    Added and linked SDL2 to the project, still need to cleanup and setup cmake on top

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+int main(int argc, char* argv[])
+{
+    printf("[MAIN] SDL Init\n");
+    int code = SDL_Init(SDL_INIT_VIDEO);
+    printf("[MAIN] return from init = %i\n", code);
+
+    SDL_Window* window = nullptr;
+    SDL_Surface* surface = nullptr;
+
+    window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    surface = SDL_GetWindowSurface(window);
+
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
