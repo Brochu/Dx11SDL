b86ae82e3fe44fdd9e3b27da2ad2b7670be87e6c
Alex Brochu <alexbrochu1@gmail.com>
2022-01-30 23:51:34 -0500

    Removed line, should merge with higher commit

--- src/Dx11Renderer.cpp
+++ src/Dx11Renderer.cpp
@@ -125,7 +125,6 @@ int Dx11Renderer::Init(HWND hWindow, UINT width, UINT height)
     // VERTEX BUFFER DESCRIPTION AND CREATION
     model = new ModelData();
     ObjReader::ReadFromFile("data/Cube.obj", *model);
-
     {
         D3D11_BUFFER_DESC vbufDesc = {};
         vbufDesc.ByteWidth = sizeof(Vertex) * model->verts.size();
