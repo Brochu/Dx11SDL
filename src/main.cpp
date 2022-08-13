776d3a18333d5e97295b676b649b361c1c4b8879
Alex Brochu <alexbrochu1@gmail.com>
2022-08-13 18:54:31 -0400

    Replace main app call to debug obj reader from the main instead. Faster iterations

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,35 @@
 #include "App.h"
+#include "ObjReader.h"
 
 int main(int argc, char* argv[])
 {
-    printf("[MAIN] Init App\n");
-    Application app("Dx11-SDL2", 1280, 720);
-    if (app.Init() != 0)
-    {
-        printf("[MAIN] Could not init application!\n");
-    }
+    // TEMP OBJ READER TESTS
+    // printf("[MAIN] Init App\n");
+    // Application app("Dx11-SDL2", 1280, 720);
+    // if (app.Init() != 0)
+    // {
+    //     printf("[MAIN] Could not init application!\n");
+    // }
 
-    while (app.isRunning)
-    {
-        if (app.Tick() != 0)
-        {
-            printf("[MAIN] Error while ticking the application!\n");
-            break;
-        }
-    }
+    // while (app.isRunning)
+    // {
+    //     if (app.Tick() != 0)
+    //     {
+    //         printf("[MAIN] Error while ticking the application!\n");
+    //         break;
+    //     }
+    // }
 
-    app.Close();
+    // app.Close();
 
-    printf("[MAIN] Terminating\n");
+    // printf("[MAIN] Terminating\n");
+    // return 0;
+
+    // TESTING MESH IMPORT
+    ObjReader::MeshData* data;
+    ObjReader::ReadSingleMeshFromFile("", &data);
+    ObjReader::DebugMeshData(*data);
+    delete data;
+
     return 0;
 }
