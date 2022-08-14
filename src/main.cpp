b1230cd177a86fa81d1cd3acf63ac5746abbfb1e
Alex Brochu <alexbrochu1@gmail.com>
2022-08-13 23:56:18 -0400

    Returned main for ObjReader tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,26 @@
 int main(int argc, char* argv[])
 {
     // TEMP OBJ READER TESTS
-    // printf("[MAIN] Init App\n");
-    // Application app("Dx11-SDL2", 1280, 720);
-    // if (app.Init() != 0)
-    // {
-    //     printf("[MAIN] Could not init application!\n");
-    // }
+    //printf("[MAIN] Init App\n");
+    //Application app("Dx11-SDL2", 1280, 720);
+    //if (app.Init() != 0)
+    //{
+    //    printf("[MAIN] Could not init application!\n");
+    //}
 
-    // while (app.isRunning)
-    // {
-    //     if (app.Tick() != 0)
-    //     {
-    //         printf("[MAIN] Error while ticking the application!\n");
-    //         break;
-    //     }
-    // }
+    //while (app.isRunning)
+    //{
+    //    if (app.Tick() != 0)
+    //    {
+    //        printf("[MAIN] Error while ticking the application!\n");
+    //        break;
+    //    }
+    //}
 
-    // app.Close();
+    //app.Close();
 
-    // printf("[MAIN] Terminating\n");
-    // return 0;
+    //printf("[MAIN] Terminating\n");
+    //return 0;
 
     // TESTING MESH IMPORT
     ObjReader::ModelData* data;
