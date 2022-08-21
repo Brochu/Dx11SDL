fc5cbc338a85a64317808ce788aef2f875aa07d7
Alex Brochu <alexbrochu1@gmail.com>
2022-08-21 00:43:12 -0400

    Bring back normal main file to test rendering with indices

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,32 @@
 
 int main(int argc, char* argv[])
 {
-    // TEMP OBJ READER TESTS
-    //printf("[MAIN] Init App\n");
-    //Application app("Dx11-SDL2", 1280, 720);
-    //if (app.Init() != 0)
-    //{
-    //    printf("[MAIN] Could not init application!\n");
-    //}
+    printf("[MAIN] Init App\n");
+    Application app("Dx11-SDL2", 1280, 720);
+    if (app.Init() != 0)
+    {
+        printf("[MAIN] Could not init application!\n");
+    }
 
-    //while (app.isRunning)
-    //{
-    //    if (app.Tick() != 0)
-    //    {
-    //        printf("[MAIN] Error while ticking the application!\n");
-    //        break;
-    //    }
-    //}
+    while (app.isRunning)
+    {
+        if (app.Tick() != 0)
+        {
+            printf("[MAIN] Error while ticking the application!\n");
+            break;
+        }
+    }
 
-    //app.Close();
+    app.Close();
 
-    //printf("[MAIN] Terminating\n");
-    //return 0;
+    printf("[MAIN] Terminating\n");
+    return 0;
 
     // TESTING MESH IMPORT
-    ObjReader::MeshData* mesh;
-    ObjReader::ReadSingleMeshFromFile("data/pagoda.obj", &mesh);
-    ObjReader::DebugMeshData(*mesh);
-    delete mesh;
+    //ObjReader::MeshData* mesh;
+    //ObjReader::ReadSingleMeshFromFile("data/pagoda.obj", &mesh);
+    //ObjReader::DebugMeshData(*mesh);
+    //delete mesh;
 
     // TESTING MODEL IMPORT
     //ObjReader::ModelData* model;
