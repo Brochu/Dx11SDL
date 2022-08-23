27db4823e4b29c87b51d0aaf704513578be827c7
Alex Brochu <alexbrochu1@gmail.com>
2022-08-23 09:50:28 -0400

    Write a bit better of main to run tests easier

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "App.h"
 #include "ObjReader.h"
 
-int main(int argc, char* argv[])
+#include <cstring>
+
+int RunModelViewer(int argc, char* argv[])
 {
     printf("[MAIN] Init App\n");
     Application app("Dx11-SDL2", 1280, 720);
@@ -23,4 +25,20 @@ int main(int argc, char* argv[])
 
     printf("[MAIN] Terminating\n");
     return 0;
+}
+
+int RunTest(int argc, char* argv[])
+{
+    // No test at the moment
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc > 1 && strcmp(argv[1], "test") == 0)
+    {
+        return RunTest(argc, argv);
+    }
+
+    return RunModelViewer(argc, argv);
 }
